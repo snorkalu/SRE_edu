60782ee53708e28c57f395328db0db814b525730
Luba Petrova <9243439@gmail.com>
2023-10-04 00:16:51 +0300

    add DB creation

---
+++ DB/init.sql
@@ -0,0 +1,14 @@
+create table if not exists public.cities
+(
+    id   bigserial,
+    name varchar(255)
+);
+
+create table if not exists public.forecast
+(
+    id          bigserial,
+    "cityId"    bigint,
+    "dateTime"  bigint,
+    temperature integer,
+    summary     text
+);
